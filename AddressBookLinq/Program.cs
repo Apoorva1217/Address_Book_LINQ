53bab2cfe85d681ea31197258297ab0d8cc0e070
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-26 19:49:19 +0530

    [Apoorva] Add . Welcome to Address Book using LINQ

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book using LINQ!");
+        }
+    }
+}
